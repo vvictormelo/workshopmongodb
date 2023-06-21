87a4092f758466a1c7773435d9e4844e8d614ff7
Victor Melo <melovictor515@gmail.com>
2023-06-20 21:40:30 -0300

    Obtendo usuário por id

--- src/main/java/com/vvictormelo/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/vvictormelo/workshopmongodb/resources/UserResource.java
@@ -1,13 +1,13 @@
 package com.vvictormelo.workshopmongodb.resources;
 
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vvictormelo.workshopmongodb.domain.User;
@@ -21,10 +21,18 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-    @GetMapping
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User obj = service.findById(id);
+        new UserDTO(obj);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
 }
--- src/main/java/com/vvictormelo/workshopmongodb/services/UserService.java
+++ src/main/java/com/vvictormelo/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.vvictormelo.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vvictormelo.workshopmongodb.domain.User;
 import com.vvictormelo.workshopmongodb.repository.UserRespository;
+import com.vvictormelo.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
