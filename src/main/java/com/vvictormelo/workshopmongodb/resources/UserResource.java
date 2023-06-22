a0317a5f0a102ca99d2859630eaa17bd55ca35d9
Victor Melo <melovictor515@gmail.com>
2023-06-21 23:28:58 -0300

    Atualização de usuário com PUT

--- src/main/java/com/vvictormelo/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/vvictormelo/workshopmongodb/resources/UserResource.java
@@ -52,4 +52,12 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDto(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
