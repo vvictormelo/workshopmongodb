ed65b149e62052edc191595b8517678ae6f4331b
Victor Melo <melovictor515@gmail.com>
2023-06-22 22:44:37 -0300

    Consulta com querie methods

--- src/main/java/com/vvictormelo/workshopmongodb/repository/PostRespository.java
+++ src/main/java/com/vvictormelo/workshopmongodb/repository/PostRespository.java
@@ -1,5 +1,7 @@
 package com.vvictormelo.workshopmongodb.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.vvictormelo.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRespository extends MongoRepository<Post, String> {
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
 
 }
