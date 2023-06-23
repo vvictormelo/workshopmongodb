f15e3e42b2f7ca3eabc9cde2bdd013eb990217bb
Victor Melo <melovictor515@gmail.com>
2023-06-22 21:57:21 -0300

    Referenciando os posts dos usuários

--- src/main/java/com/vvictormelo/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/vvictormelo/workshopmongodb/config/Instantiation.java
@@ -42,6 +42,9 @@ public class Instantiation implements CommandLineRunner {
                 new AuthorDTO(maria));
 
         postRespository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 
 }
