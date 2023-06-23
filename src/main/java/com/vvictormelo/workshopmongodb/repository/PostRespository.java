8d4f2e286c5bfac2673c9d74506de60922af3591
Victor Melo <melovictor515@gmail.com>
2023-06-22 22:57:24 -0300

    Consulta simples com @Query

--- src/main/java/com/vvictormelo/workshopmongodb/repository/PostRespository.java
+++ src/main/java/com/vvictormelo/workshopmongodb/repository/PostRespository.java
@@ -3,12 +3,16 @@ package com.vvictormelo.workshopmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.vvictormelo.workshopmongodb.domain.Post;
 
 @Repository
 public interface PostRespository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
